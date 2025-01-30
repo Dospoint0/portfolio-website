cca64948ad81e8df372fa72a2bafbd3b2a3e54f2
Naif Lohani <n.lohani2002@gmail.com>
2025-01-29 18:47:57 -0700

    Update .gitattributes to enforce consistent line endings for all files

---
+++ src/test/java/com/naiflohani/project_portfolio/ProjectPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naiflohani.project_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
